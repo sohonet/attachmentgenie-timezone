3874edd84abbb550badf281bd3a03ade12d6ccf6
Bram Vogelaar <attachmentgenie@inuits.eu>
2014-07-30 21:55:07 +0200

    making timezone updates work on centos

--- Modulefile
+++ Modulefile
@@ -1,13 +1,11 @@
 name 'attachmentgenie-timezone'
-version '1.0.2'
+version '1.1.0'
 
 author 'Eivind Uggedal <eivind@uggedal.com>'
 license 'MIT License'
 project_page 'https://github.com/attachmentgenie/puppet-module-timezone'
 source 'git://github.com/attachmentgenie/puppet-module-timezone.git'
 summary 'Puppet timezone Module'
-description 'Module for configuring timezone.
-
-Tested on Debian GNU/Linux 6.0 Squeeze and Ubuntu 12.04 LTS with Puppet 2.7.
+description 'Module for configuring timezone.'
 Patches for other operating systems are welcome.'
 dependency 'puppetlabs/stdlib', '>=2.2.1'
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,24 @@ class timezone($zone='UTC') {
     content => inline_template('<%= @zone + "\n" %>'),
   }
 
-  exec { 'reconfigure-tzdata':
-    command     => '/usr/sbin/dpkg-reconfigure -f noninteractive tzdata',
-    subscribe   => File['/etc/timezone'],
-    require     => File['/etc/timezone'],
-    refreshonly => true,
+  case $::osfamily {
+    'debian': {
+      exec { 'reconfigure-tzdata':
+        command     => '/usr/sbin/dpkg-reconfigure -f noninteractive tzdata',
+        subscribe   => File['/etc/timezone'],
+        require     => File['/etc/timezone'],
+        refreshonly => true,
+      }
+    }
+    'redhat': {
+      file { '/etc/localtime':
+        ensure  => 'present',
+        target  => "/usr/share/zoneinfo/$zone",
+        require => Package['tzdata'],
+      }
+    }
+    default: {
+      fail("Unsupported osfamily ${::osfamily}")
+    }
   }
 }
